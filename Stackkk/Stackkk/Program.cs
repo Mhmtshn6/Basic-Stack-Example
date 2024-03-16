8a9097281e3954dcdca32ecd34efccb34d546024
Mhmtshn6 <mehmet68064@gmail.com>
2024-03-17 01:58:25 +0300

    Basic Stack Example
    Simple example of Stack, one of the one of the linear data structures

---
+++ Stackkk/Stackkk/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stackkk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(10);
+            stack.Push(20);
+            stack.Push(30);
+            // Add Number to Stack
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            // Print Stack Contents
+
+            object topNum = stack.Pop();
+            Console.WriteLine(topNum.ToString());
+            // Print Top Number to Stack
+
+            object poppedNum = stack.Pop();
+            Console.WriteLine(poppedNum.ToString());
+            // Deleted Top Number
+
+            Console.WriteLine(stack.Count);
+
+            int[] ArrayNum = stack.ToArray();
+            // Carries Stack to Array
+
+            Console.ReadLine();
+        }
+    }
+}
